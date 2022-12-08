1317d22f86d0feac39dee60ed71aaffb2be3d62d
LotteVeerman <118219284+LotteVeerman@users.noreply.github.com>
2022-12-08 14:22:35 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,82 @@
 // JavaScript Document
-console.log("hi");
+
+
+/******************************/
+/* menu openen de MENU button */
+/******************************/
+
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+var openButton = document.querySelector("header > button");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+openButton.addEventListener("click", openMenu);
+
+// stap 3: voeg in de functie een class toe aan de nav
+function openMenu() {  
+  // zoek de nav op
+  var deNav = document.querySelector("nav");
+  // voeg class toe aan nav
+  deNav.classList.add("toonMenu");
+}
+
+
+
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+
+// stap 1 - zoek sluiten button op
+var sluitButton = document.querySelector("nav button");
+
+// stap 2 - laat die button luisteren naar kliks
+sluitButton.addEventListener("click", sluitMenu);
+
+// stap 3 - in de functie verwijder de class van de nav
+function sluitMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
+
+
+
+
+/**********************************/
+/* bonus: menu sluiten met escape */
+/**********************************/
+window.addEventListener("keydown", handleKeydown);
+
+function handleKeydown(event) {
+  if (event.key == "Escape") {
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
+}
+
+
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  // Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
